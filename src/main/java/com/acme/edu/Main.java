22e91e299cb781c44853daef7a17f3e31c7c5af7
Yuriy Zhayvoronok <Yuriy Zhayvoronok>
2015-10-28 16:55:29 +0300

    Meaven Environment Test

---
+++ src/main/java/com/acme/edu/Main.java
@@ -0,0 +1,8 @@
+package com.acme.edu;
+
+public class Main {
+	public static void main(String[] argv){
+		com.acme.edu.present.Printer.print("Hello World");
+	}
+}
+
---
+++ src/main/java/com/acme/edu/present/Printer.java
@@ -0,0 +1,7 @@
+package com.acme.edu.present;
+
+public class Printer{
+	public static void print(String message){
+		System.out.println(message);
+	}
+}
