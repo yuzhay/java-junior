065007d7ccfffe5f9b6e43625ff2606b651c2087
Yuriy <yura@zhayvoronok.com>
2015-10-29 16:58:02 +0300

    Comments

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,39 +2,78 @@ package com.acme.edu;
 
 public class Logger {
 
+    //region fields
+    public static final String LOG_PRIMITIVE = "primitive";
+    public static final String LOG_CHAR = "char";
+    public static final String LOG_STRING = "string";
+    public static final String LOG_REFERENCE = "reference";
+    //endregion
+
+    //region entry point
+
+    /**
+     * Entry Point
+     *
+     * @param argv class arguments
+     */
+    public static void main(String[] argv) {
+
+    }
+
+    //endregion
+
+    //region public methods
+
+    /**
+     * Print integer parameter to log
+     *
+     * @param message print parameter
+     */
     public static void log(int message) {
-        print("primitive: " + message);
+        print(String.format("%s: %s", Logger.LOG_PRIMITIVE, message));
     }
 
-    public static void log(byte message) {
-        print("primitive: " + message);
+    /**
+     * Print char parameter to log
+     *
+     * @param message print parameter
+     */
+    public static void log(char message) {
+        print(String.format("%s: %s", Logger.LOG_CHAR, message));
     }
 
-    public static void log(char c) {
-        print("char: " + c);
+    /**
+     * Print boolean parameter to log
+     *
+     * @param message print parameter
+     */
+    public static void log(boolean message) {
+        print(String.format("%s: %s", Logger.LOG_PRIMITIVE, message));
     }
 
-    public static void log(boolean b) {
-        String str = "true";
-        if (!b) {
-            str = "false";
-        }
-        print("primitive: " + str);
+    /**
+     * Print String parameter to log
+     *
+     * @param message print parameter
+     */
+    public static void log(String message) {
+        print(String.format("%s: %s", Logger.LOG_STRING, message));
     }
 
-    public static void log(String str) {
-        print("string: " + str);
+    /**
+     * Print Object parameter to log
+     *
+     * @param message print parameter
+     */
+    public static void log(Object message) {
+        print(String.format("%s: %s", Logger.LOG_REFERENCE, message));
     }
 
-    public static void log(Object o) {
-        print("reference: " + o.toString());
-    }
+    //endregion
 
+    //region private methods
     private static void print(String str) {
         System.out.println(str);
     }
-
-    public static void main(String[] argv) {
-        System.out.println("Test");
-    }
+    //endregion
 }
